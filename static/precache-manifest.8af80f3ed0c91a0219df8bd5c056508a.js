99d1b65ef5b628bf4aa92369755ccc43953daf62
horingring <59082416+horingring@users.noreply.github.com>
2021-02-16 21:06:39 +0900

    Updates

---
+++ static/precache-manifest.8af80f3ed0c91a0219df8bd5c056508a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a1c6d358f1cb5840985d8fd38c6600",
+    "url": "/chatbot_service/index.html"
+  },
+  {
+    "revision": "7091cbb95a96b7f2e509",
+    "url": "/chatbot_service/static/css/2.6109648b.chunk.css"
+  },
+  {
+    "revision": "19288f8a628f4ccc6a5f",
+    "url": "/chatbot_service/static/css/main.e5b54b6b.chunk.css"
+  },
+  {
+    "revision": "7091cbb95a96b7f2e509",
+    "url": "/chatbot_service/static/js/2.dd6512a4.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/chatbot_service/static/js/2.dd6512a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19288f8a628f4ccc6a5f",
+    "url": "/chatbot_service/static/js/main.9cd898c9.chunk.js"
+  },
+  {
+    "revision": "f030e8d0416085017533",
+    "url": "/chatbot_service/static/js/runtime-main.9d877434.js"
+  },
+  {
+    "revision": "71e25eb23bcdc52ad7848cb19bb05f58",
+    "url": "/chatbot_service/static/media/JSDongkang-Bold.71e25eb2.ttf"
+  },
+  {
+    "revision": "4324f4a626474e0ecf1f78041fa78984",
+    "url": "/chatbot_service/static/media/JSDongkang-Regular.4324f4a6.ttf"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/chatbot_service/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/chatbot_service/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/chatbot_service/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/chatbot_service/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "2727cea56502f76a08846df1525ccdda",
+    "url": "/chatbot_service/static/media/cooperation.2727cea5.jpg"
+  },
+  {
+    "revision": "8733071f9c83fee87a401f1dc5896f55",
+    "url": "/chatbot_service/static/media/iphone.8733071f.jpg"
+  },
+  {
+    "revision": "1f2054e7ce18ec2f427a966ee9af9f06",
+    "url": "/chatbot_service/static/media/lightbulb.1f2054e7.jpg"
+  },
+  {
+    "revision": "bdfdee9f98ef53c4875d13a7bbf3fab4",
+    "url": "/chatbot_service/static/media/lightbulb_logo.bdfdee9f.jpg"
+  },
+  {
+    "revision": "5a2a1424d0b4ead42ca0e90c2f88d90a",
+    "url": "/chatbot_service/static/media/network.5a2a1424.jpg"
+  },
+  {
+    "revision": "c4b242588d12d9c59e8de83143258cfe",
+    "url": "/chatbot_service/static/media/network_logo.c4b24258.jpg"
+  },
+  {
+    "revision": "15d69df412cf35b1f9940f8950234f38",
+    "url": "/chatbot_service/static/media/pizza.15d69df4.jpg"
+  },
+  {
+    "revision": "beab1acc401d9b3463b8123a6213efc6",
+    "url": "/chatbot_service/static/media/pizza2.beab1acc.jpg"
+  },
+  {
+    "revision": "fa8b28860419a8ae320651c7406875b2",
+    "url": "/chatbot_service/static/media/pizza_intro.fa8b2886.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/chatbot_service/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/chatbot_service/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/chatbot_service/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/chatbot_service/static/media/slick.f97e3bbf.svg"
+  }
+]);
